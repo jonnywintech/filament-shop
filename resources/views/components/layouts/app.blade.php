1eec332d1c62cde8d9ee5f7d0b4d616cbe6dd32d
Nikola Stankovic <jonnywintech@gmail.com>
2024-07-29 13:20:33 +0200

    initial html pages added

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,14 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>{{ $title ?? 'Shopmania' }}</title>
-    @vite(['resources/js/app.js','resources/css/app.css'])
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
     @livewireStyles
 </head>
 
 <body class="bg-slate-200 dark:bg-slate-700">
-
+    @livewire('partials.navbar')
     {{ $slot }}
 
+    @livewire('partials.footer')
     @livewireScriptConfig
 </body>
 
