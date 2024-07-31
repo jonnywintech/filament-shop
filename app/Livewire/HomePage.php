23463f2698eae3baa0d7933e49f2fb025cdb8c96
Nikola Stankovic <jonnywintech@gmail.com>
2024-07-31 10:21:44 +0200

    adding title to livewire

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -3,11 +3,14 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+#[Title('Home Page - Shopmania')]
 class HomePage extends Component
 {
     public function render()
     {
+
         return view('livewire.home-page')
         ->layout('components.layouts.app');
     }
