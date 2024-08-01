b0f7ca557cb22a565204dfb53d2fd960111d74f4
Nikola Stankovic <jonnywintech@gmail.com>
2024-08-01 12:32:33 +0200

    listing single product

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,12 +2,26 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+#[Title('Product Detail - Shopmania')]
 class ProductDetailPage extends Component
 {
+    public $slug;
+
+    public function mount($slug)
+    {
+        $this->slug = $slug;
+    }
+
     public function render()
     {
-        return view('livewire.product-detail-page');
+
+
+        return view('livewire.product-detail-page', [
+            'product' => Product::where('slug', $this->slug)->firstOrFail(),
+        ]);
     }
 }
