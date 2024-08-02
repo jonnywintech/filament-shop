b9a603fb7e49963de981c07fa9ce28d6d7ed0b67
Nikola Stankovic <jonnywintech@gmail.com>
2024-08-02 16:13:45 +0200

    updated single product detailss

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -3,17 +3,48 @@
 namespace App\Livewire;
 
 use App\Models\Product;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
+use App\Helpers\CartManagment;
 use Livewire\Attributes\Title;
+use App\Livewire\Partials\Navbar;
 
 #[Title('Product Detail - Shopmania')]
 class ProductDetailPage extends Component
 {
+    use LivewireAlert;
     public $slug;
+
+    public $quantity = 1;
 
     public function mount($slug)
     {
         $this->slug = $slug;
+    }
+
+    public function increaseQty()
+    {
+        $this->quantity++;
+    }
+    public function decreaseQty()
+    {
+        if ($this->quantity <= 1) {
+            return;
+        }
+        $this->quantity--;
+    }
+
+    public function addToCart($id)
+    {
+        $total_count = CartManagment::addItemsToCart($id);
+
+        $this->dispatch('update-cart-count', total_count: $total_count)->to(Navbar::class);
+
+        $this->alert('success', 'Product added to the cart successfully!', [
+        'position' => 'bottom-end',
+        'timer' => 1000,
+        'toast' => true,
+        ]);
     }
 
     public function render()
