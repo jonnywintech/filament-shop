621876dc8d28f674506d55028fd33299062bb71f
Nikola Stankovic <jonnywintech@gmail.com>
2024-08-07 15:53:42 +0200

    fronted for place order is finished now backend logic remaining

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -2,12 +2,43 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagment;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Checkout')]
 class CheckoutPage extends Component
 {
+
+    public string
+        $first_name,
+        $last_name,
+        $phone,
+        $address,
+        $city,
+        $state,
+        $zip,
+        $payment_method;
+
+    public function checkout()
+    {
+       $this->validate([
+        'first_name' => 'required|string|min:3|max:255',
+        'last_name' => 'required|string|min:3|max:255',
+        'phone' => 'required|numeric',
+        'address' => 'required',
+        'city' => 'required',
+        'state' => 'required',
+        'zip' => 'required',
+        'payment_method' => 'required'
+       ]);
+
+       $cart_items = CartManagment::getCartItemsFromCookie();
+    }
     public function render()
     {
-        return view('livewire.checkout-page');
+        $cart_items = CartManagment::getCartItemsFromCookie();
+        $grand_total = CartManagment::calculateGrandTotal($cart_items);
+        return view('livewire.checkout-page', compact('cart_items', 'grand_total'));
     }
 }
