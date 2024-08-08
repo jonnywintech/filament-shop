49409c9f3ad3b7aa110b79ea11cbf18db7ab3429
Nikola Stankovic <jonnywintech@gmail.com>
2024-08-08 15:33:33 +0200

    added order details multiple page details

--- app/Livewire/MyOrderDetailPage.php
+++ app/Livewire/MyOrderDetailPage.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Order;
 use Livewire\Component;
 
 class MyOrderDetailPage extends Component
 {
+    public $order;
+
+    public function mount($order)
+    {
+        $this->order = $order;
+    }
+
+
     public function render()
     {
-        return view('livewire.my-order-detail-page');
+        $order_details = Order::with('address')->with('items')->find($this->order);
+        return view('livewire.my-order-detail-page', compact('order_details'));
     }
 }
--- app/Livewire/MyOrdersPage.php
+++ app/Livewire/MyOrdersPage.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire;
 
+use App\Models\Order;
 use Livewire\Component;
 
 class MyOrdersPage extends Component
 {
     public function render()
     {
-        return view('livewire.my-orders-page');
+        $orders = Order::where('user_id', auth()->user()->id)->orderBy('created_at','desc')->paginate(10);
+
+        return view('livewire.my-orders-page', compact('orders'));
     }
 }
