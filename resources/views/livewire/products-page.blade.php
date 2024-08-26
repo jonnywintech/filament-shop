c321bcd2f1c413973d3bc4826df4833269f573e1
Nikola Stankovic <jonnywintech@gmail.com>
2024-08-26 09:47:45 +0200

    cleanup

--- resources/views/livewire/products-page.blade.php
+++ resources/views/livewire/products-page.blade.php
@@ -94,7 +94,6 @@
                                 <div class="border border-gray-300 dark:border-gray-700">
                                     <div class="relative bg-gray-200">
                                         <a href="/products/{{ $product->slug }}" class="">
-                                            {{-- @dd($product->images[0]) --}}
                                             <img src="{{ url('storage/' . $product->images[0]) }}" alt=""
                                                 class="object-cover w-full h-56 mx-auto ">
                                         </a>
