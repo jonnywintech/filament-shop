e92676bb82a75e29a31b369f50f953a1c0d81d7f
Nikola Stankovic <jonnywintech@gmail.com>
2024-09-03 15:29:19 +0200

    fixed naming by psr

--- app/View/Components/InvoiceLayout.php
+++ app/View/Components/InvoiceLayout.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class invoice-layout extends Component
+class InvoiceLayout extends Component
 {
     /**
      * Create a new component instance.
